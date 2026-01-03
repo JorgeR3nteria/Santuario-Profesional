f1b8837049971c219a46ddbf8884cdaf3d45fc3a
JorgeR3nteria <62585427+JorgeR3nteria@users.noreply.github.com>
2026-01-03 00:01:57 -0500

    Se realizan cambios en el reto 3

--- Aprender Programación/Python/Retos/Reto3.py
+++ Aprender Programación/Python/Retos/Reto3.py
@@ -4,11 +4,22 @@
 # Devuelve True si es par, False si no
 # Luego úsala para imprimir "Par" o "Impar"
 
+# def es_par(numero):
+#     if numero %2 ==0:
+#         return "Par"
+#     else:
+#         return "impar"
+# valor = es_par(numero = int(input("Digita un numero: ")))
+# resultado= valor
+# print(resultado)
+
+# Versión corregida
 def es_par(numero):
-    if numero %2 ==0:
-        return "Par"
-    else:
-        return "impar"
-valor = es_par(numero = int(input("Digita un numero: ")))
-resultado= valor
-print(resultado)
+    return numero % 2 == 0
+
+numero = int(input("Digita un numero: "))
+
+if es_par(numero):
+    print("Par")
+else:
+    print("Impar")
